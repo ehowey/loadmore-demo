fa53ff68073a43264cff4c30cebb8b4ad149ca00
ehowey <ehowey@gmail.com>
2020-12-21 06:00:14 -0700

    Initial commit from gatsby: (https://github.com/ehowey/gatsby-starter-catalyst-core.git)

---
+++ gatsby-config.js
@@ -0,0 +1,76 @@
+module.exports = {
+  siteMetadata: {
+    title: `Catalyst Core`,
+    description: `Speed up your GatsbyJS development workflow. Designed as a set of opinionated and advanced themes and starters using MDX and Theme-UI. Incorporates only gatsby-theme-catalyst-core.`,
+    keywords: [`gatsby`, `theme`, `react`, `catalyst`],
+    author: `Eric Howey`,
+    siteUrl: `https://gatsby-starter-catalyst-core.netlify.app`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
+    menuLinks: [
+      {
+        name: `Page 1`,
+        link: `/page-1`,
+        type: `internal`, //internal or anchor
+      },
+      {
+        name: `Anchor 1`,
+        link: `#anchor-1`,
+        type: `anchor`, //internal or anchor
+      },
+      {
+        name: `Page 2`,
+        link: `/page-2`,
+        type: `internal`, //internal or anchor
+      },
+    ],
+    socialLinks: [
+      {
+        name: `Email`,
+        link: `eric@erichowey.dev`,
+        location: `footer`, //Options are "all", "header", "footer"
+      },
+      {
+        name: `Github`,
+        link: `https://www.github.com/ehowey`,
+        location: `all`, //Options are "all", "header", "footer"
+      },
+      {
+        name: `Twitter`,
+        link: `https://www.twitter.com/erchwy`,
+        location: `header`, //Options are "all", "header", "footer"
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-catalyst-core`,
+      options: {
+        //Default options are:
+        // contentPath: `content/pages`,
+        // assetPath: `content/assets`,
+        // displaySiteLogo: true,
+        // displaySiteTitle: true,
+        // displaySiteLogoMobile: true,
+        // displaySiteTitleMobile: true,
+        // invertLogo: false,
+        // useStickyHeader: false,
+        // useSocialLinks: true,
+        // useColorMode: true,
+        // useKatex: false,
+        // footerContentLocation: "left", // "left", "right", "center"
+        // remarkImagesWidth: 1440,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-catalyst`,
+        short_name: `catalyst`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#cccccc`,
+        display: `minimal-ui`,
+        icon: `content/assets/catalyst-site-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
